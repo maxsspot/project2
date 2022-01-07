a25ee1c20a64d3af405d3240c065c426526bb0a1
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2022-01-07 16:15:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -71,3 +71,27 @@ function go () {
                       localStorage.setItem("submitted", "true");
                       sessionStorage.setItem("submitted2", "true");
            }
+
+var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+
+var x = setInterval(function() {
+
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById ("overlay").style.transform = "scale(0.0)";
+  }
+}, 1000);
