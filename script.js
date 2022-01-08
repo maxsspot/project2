980a5f005f130cab9bebc77269a8535b4b6e2dfc
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2022-01-08 11:45:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-document.getElementById ("nav").style.display = "none";
-document.getElementById ("home").style.display = "none";
 var sum = localStorage.getItem('submitted');
 var sumb = sessionStorage.getItem('submitted2');
 if (sum.value = "true") {
@@ -72,29 +70,3 @@ function go () {
                       localStorage.setItem("submitted", "true");
                       sessionStorage.setItem("submitted2", "true");
            }
-
-var countDownDate = new Date("Jan 9, 2021 14:05:00").getTime();
-
-var x = setInterval(function() {
-
-
-  var now = new Date().getTime();
-
-  var distance = countDownDate - now;
-
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-  
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById ("overlay").style.transform = "scale(0.0)";
-    document.getElementById ("nav").style.display = "block";
-    document.getElementById ("home").style.display = "block";
-  }
-}, 1000);
